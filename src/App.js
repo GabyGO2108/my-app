9bf10443678a2a13718862707efabc534a9bc33c
GabyGO2108 <gogabrielagutierrezo02@gmail.com>
2021-02-11 17:25:39 -0600

    .Css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 
 import Search from "./Search";
+import './Weather.css';
 import './App.css';
 
-function App() {
+export default function Wetaher() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
+     
+    <div className="weather-app">
+      <div>
+       
         <h1>
           Weather app
         </h1>
         
         <Search />
       
-      </header>
+      </div>
+    
 
-      <footer> <small>
+    <div className="row">
+       <h3> <span class="temperature" id="temperature"></span
+            ><span class="units">
+              <small> <a href="#" id="celsius-link">°C</a> |
+              <a href="#" id="fahrenheit-link">°F</a>
+          </small>
+          </h3>
+        
+         
+         <footer> <small>
       <a href="https://github.com/GabyGO2108" rel="noreferrer" target="_blank">
         Open source code
       </a>
       by Gaby Gutiérrez
     </small></footer>
+
+   </div>
+
     </div>
   );
-}
+
 
 export default App;
