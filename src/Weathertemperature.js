bd7e4c3dde153b5f281b078b74d5e84daa64618f
GabyGO2108 <gogabrielagutierrezo02@gmail.com>
2021-03-12 15:05:16 -0600

    Update Weathertemperature.js

--- src/Weathertemperature.js
+++ src/Weathertemperature.js
@@ -1,47 +1,45 @@
+  
 import React, { useState } from "react";
 
-export default function Weathertemperature(props) {
-    const { unit, setUnit } = useState("celcius");
-    function convertToFahrenheit(event) {
-        event.preventDefault();
-        setUnit("fahrenheit");
-        
-    }
-    function convertToCelius(event) {
-        event.preventDefault();
-        setUnit("celsius");
-    }
-    let fahrenheit = (props.celcius * 9 / 5) + 32;
-    
-    if (unit === 'celcius') { 
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
     return (
-        <div>
-            <span className="temperature">
-                Temperature:{Math.round(weather.celcius)}°C
-                 </span>
-                <span className="unit"> 
-                <a href="/" onClick={convertToFahrenheit}>
-                    °C </a>
-            </span>
-            
-    
-        </div>          
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
     );
-    } else {
-        
-        return (
-            <div>
-            <span className="temperature">
-                Temperature:{Math.round(weather.fahrenheit)}
-                 </span>
-                <span className="unit"> °F |{" "}
-                <a href="/" onClick={convertToCelcius}>
-                    °C </a>
-            </span>
-            
-    
-        </div> 
-        )
-    
-    }
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
 }
