e1731d47450ba238eb64713b82f418c72888fb03
GabyGO2108 <gogabrielagutierrezo02@gmail.com>
2021-03-12 15:04:49 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,23 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-import Search from "./Search";
-import './Weather.css';
-import FormatDate from './FormatDate';
-
-
-
-export default function App () {
+export default function App() {
   return (
-     
-    <div className="weather-app">
-      <div>
-       
-        <h1>
-          Weather app
-        </h1>
-
-        <h2 className="city"> {Weather.city} </h2>
-        
-        
-        <h3><FormatDate date = {Weather.date} /> </h3>
-
-        
-     
-        
-       
-      
-      </div>
-      
-      <div className="row">
-      <div className="col-6">
-       <div class="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt="Clear sky"
-              className="float-left"
-              
-              
-            />
-            
-        </div>
-        </div>
-        </div>
-    
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Tokyo" />
 
         <footer> <small>
            Open-sourced on {" "}
       <a href="https://github.com/GabyGO2108/my-app"  target="_blank">
         GitHub 
       </a> {" "}
-       by Gaby Gutiérrez
+       by Gaby Gutiérrez-Olguín
     </small></footer>
 
 
+      </div>
       </div>
 
 
